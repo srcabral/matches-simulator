5e30305ee30edc71874ef44d187facca72d877cb
Sergio_Cabral <scastrocabral@gmail.com>
2022-01-18 16:01:12 -0300

    Create project

---
+++ app/src/main/java/br/com/srcabral/matchessimulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.srcabral.matchessimulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
